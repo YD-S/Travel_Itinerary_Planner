f68aba61c3ea6a52276d8651d73ce47d5a098feb
Yash <seehra.a@yahoo.com>
2025-05-15 15:14:36 +0200

    added backend init

---
+++ backend/src/main/java/com/yash/YD_S/travel_planner_backend/TravelPlannerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yash.YD_S.travel_planner_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPlannerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelPlannerBackendApplication.class, args);
+	}
+
+}
