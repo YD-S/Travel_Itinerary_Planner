55cc00a7e76ce24be7c60a5d6f2c56bbb6f80eb2
Charlotte <charlotteschnx@gmail.com>
2025-05-18 16:33:26 +0200

    Added some repositories

---
+++ backend/src/main/java/com/yash/YD_S/travel_planner_backend/repository/AccommodationRepository.java
@@ -0,0 +1,10 @@
+package com.yash.YD_S.travel_planner_backend.repository;
+
+import com.yash.YD_S.travel_planner_backend.model.Accommodation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccommodationRepository extends JpaRepository<Accommodation, Long> {
+    Optional<Accommodation> findByAccommodationName(String accommodationName);
+}
