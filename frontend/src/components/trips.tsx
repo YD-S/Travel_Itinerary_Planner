7d2145ee7241ad35e18f916745fb3efe71461fc4
Charlotte <charlotteschnx@gmail.com>
2025-06-04 23:47:49 +0200

    added trips to frontend

--- frontend/src/components/trips.tsx
+++ frontend/src/components/trips.tsx
@@ -1,16 +1,17 @@
+import type { AxiosResponse } from 'axios';
 import api from '../api/axios.ts';
-import { useEffect, useRef } from 'react';
+import {useEffect, useRef} from 'react';
 
 function DisplayTrips() {
     const called = useRef(false);
-
+    let response: AxiosResponse<any, any>
     useEffect(() => {
         if (called.current) return;
         called.current = true;
 
         const fetchTrips = async () => {
             try {
-                const response = await api.get('api/trip/', {
+                response = await api.get('api/trip/', {
                     headers: { "Authorization": `Bearer ${localStorage.getItem("authToken")}` }
                 });
                 console.log(response.data);
@@ -21,7 +22,13 @@ function DisplayTrips() {
         fetchTrips();
     }, []);
 
-    return null;
+    return (
+        <div className="container">
+            {response.data.map((trip) => (
+                <p>{trip.title}</p>
+            ))}
+        </div>
+    );
 }
 
 export default DisplayTrips;
